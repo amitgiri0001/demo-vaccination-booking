d7676a2c42854bedf69cc6ee571fe931dd7d986f
Amit Giri <amit.giri0001@gmail.com>
2021-08-25 15:18:00 +0800

    chore: http in and out logs

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,3 +1,17 @@
-import {MiddlewareSequence} from '@loopback/rest';
+import {MiddlewareSequence, RequestContext} from '@loopback/rest';
+import moment from 'moment';
+export class MySequence extends MiddlewareSequence {
+  log(msg: string) {
+    console.info(msg);
+  }
 
-export class MySequence extends MiddlewareSequence {}
+  async handle(context: RequestContext) {
+    this.log(
+      `\n ${moment().utc().format()} [HTTP-IN] ${context.request.originalUrl}`,
+    );
+    await super.handle(context);
+    this.log(
+      `\n ${moment().utc().format()} [HTTP-OUT] ${context.request.originalUrl}`,
+    );
+  }
+}
